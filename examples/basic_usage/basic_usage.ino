51f126526f58bbe7c8766b1fde112f3de5cb972b
Chris--A <chris@genx.biz>
2015-08-20 13:41:43 +1000

    Updated codebase and example.﻿

--- examples/basic_usage/basic_usage.ino
+++ examples/basic_usage/basic_usage.ino
@@ -1,16 +1,35 @@
 #include <PrintEx.h>
 
 void setup() {
-  char buff[ 100 ];
+
+
+  /***  NOTE: See the readme for the features supported in sprintf/printf ***/
+  
   
   Serial.begin( 9600 );
   
-  //GString g( buff );
-  //g.printf( "%17n\nFlash string [%p]\nA decimal:%f\nDEC: %ld\nHEX: %lx\n%17n", '-', F("Hi from flash!"), 3.1415f, 65000, 65000, '-' );
-  //Serial.print( g );
+  //Printing a formatted string to a RAM buffer.
+  char buff[ 100 ];
+  GString g( buff );
+  g.printf( "%17n\nFlash string [%p]\nA decimal:%f\nDEC: %ld\nHEX: %lx\n%17n", '-', F("Hi from flash!"), 3.1415f, 65000, 65000, '-' );
   
-  AnyPrinter serial( Serial );
+  //Output result
+  Serial.print( g );
+  
+  
+  //Using formatted stirngs without a buffer.
+  PrintEx serial = Serial ; //Wrap the Serial object in a PrintEx interface.
+  
   serial.printf( "%17n\nFlash string [%p]\nA decimal:%f\nDEC: %ld\nHEX: %lx\n%17n", '-', F("Hi from flash!"), 3.1415f, 65000, 65000, '-' );
+  
+  /***
+  For streaming communication, you can use StreamEx.
+  It allows usage of standard stream functions like read(), parseInt(),...
+  just with the PrintEx features included.
+  
+  StreamEx serial = Serial;
+  
+  ***/
   
 }
 
