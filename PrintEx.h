2b1263e829e46dbf2ee351cd2a61476426e6cdaa
Chris--A <chris@genx.biz>
2015-08-21 21:59:32 +1000

    Updated structure for 1.5 library spec.﻿

--- PrintEx.h
+++
@@ -1,1 +0,0 @@
-/********************************************************************************    Copyright 2015 Christopher Andrews.    https://github.com/Chris--A/PrintEx    Released under MIT licence.********************************************************************************/  #include "MemoryPrinter.h"
---
+++ src/PrintEx.h
@@ -0,0 +1,1 @@
+/********************************************************************************    Copyright 2015 Christopher Andrews.    https://github.com/Chris--A/PrintEx    Released under MIT licence.********************************************************************************/  #include "lib/MemoryPrinter.h"
