77747137eda63765cd1db034dac1714b4885a952
Chris--A <chris@genx.biz>
2015-08-30 22:36:28 +1000

    Add PrintConcat interface. This change removes the need for a Print base by using static polymorphism.﻿

---
+++ src/lib/PrintConcat.h
@@ -0,0 +1,37 @@
+/********************************************************************************
+    Copyright 2015 Christopher Andrews.
+    https://github.com/Chris--A/PrintEx
+
+    Released under MIT licence.
+********************************************************************************/
+
+//Uses CRTP macros.
+#include "Globals.h"
+
+#ifndef HEADER_CONCAT
+    #define HEADER_CONCAT
+	
+	template< typename derived >
+		struct PrintConcat{
+		
+            template< typename T >
+                _INLINE_ derived &operator +=( const T &t )
+                    { return CRTPO.concat( t ); }
+
+            template< typename T >
+                _INLINE_ derived &concat( const T &t )
+                    { return CRTPO.print( t ), CRTPP; }
+
+            template< typename T >
+                _INLINE_ derived &concat( const T &t, const int i )
+                    { return CRTPO.print( t, i ) CRTPP; }
+
+            template< typename T >
+                _INLINE_ derived &concatln( const T &t )
+                    { return CRTPO.println( t ), CRTPP; }
+
+            template< typename T >
+                _INLINE_ derived &concatln( const T &t, const int i )
+                    { return CRTPO.println( t, i ), CRTPP; }
+		};
+#endif
