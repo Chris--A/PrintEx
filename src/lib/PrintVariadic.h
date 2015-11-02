f86c455caf5969de09f1df6af09c96a184394b73
Chris--A <chris@genx.biz>
2015-11-02 23:37:03 +1000

    Fixed PrintVariadic implementation.﻿

--- src/lib/PrintVariadic.h
+++ src/lib/PrintVariadic.h
@@ -16,12 +16,17 @@
         template< typename derived >
             struct PrintVariadic{
 
-                derived &printx( void ){ return CRTPO; }
+				template< typename S >
+                derived &printx_continue(  S s ){ return CRTPO; }
 
                 template< typename T, typename... U >
                     derived &printx( const T& t, const U&... u ){
-                        CRTPO << t;
-                        return printx( u... );
+                        return printx_continue( CRTPO << t, u... );
+                    }
+					
+                template< typename S, typename T, typename... U >
+                    derived &printx_continue( S s, const T& t, const U&... u ){
+                        return printx_continue( s << t, u... );
                     }
         };
     #else
