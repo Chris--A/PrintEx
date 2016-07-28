75d3b77af7dfa646b7911a88bd83cb76f957bc6b
Chris--A <chris@genx.biz>
2016-07-29 04:08:58 +1000

    Added start of StreamExWrap

--- src/lib/StreamExtension.h
+++ src/lib/StreamExtension.h
@@ -40,6 +40,9 @@
         pft printf( const char *format, ... );
     };
 
+	//Forward declaration as StreamEx uses StreamExWrap
+    template<typename T> struct StreamExWrap;
+
     struct StreamEx : StreamExtension{
 
         StreamEx( Stream &in ) : stream(in) {}
@@ -48,7 +51,71 @@
         int peek( void )                    { return stream.peek(); }
         int read( void )                    { return stream.read(); }
         size_t write( uint8_t data )        { return stream.write(data); }
+		
+		template<typename T>
+		static StreamExWrap<T> &wrap( T &t ){
+			return *transform<StreamExWrap<T>*, T*>(&t);
+		}
+
         Stream &stream;
     };
+	
+    template<typename T>
+    struct StreamExWrap :
+            PrintConcat< StreamExWrap<T> >,
+            PrintRepeat< StreamExWrap<T> >,
+            ios::OStreamBase< StreamExWrap<T> >,
+			ios::IStreamBase< StreamExWrap<T> >,
+            PrintVariadic< StreamExWrap<T> >,
+            T{
 
+        /***
+            Constructors are provided to allow passing through parameters to wrapped type.
+        ***/
+        StreamExWrap() : T() {}
+
+        template<typename A> StreamExWrap( A a ) : T(a) {}
+        template<typename A, typename B> StreamExWrap( A a, B b ) : T(a, b) {}
+        template<typename A, typename B, typename C> StreamExWrap( A a, B b, C c ) : T(a, b, c) {}
+        template<typename A, typename B, typename C, typename D> StreamExWrap( A a, B b, C c, D d ) : T(a, b, c, d) {}
+        template<typename A, typename B, typename C, typename D, typename E> StreamExWrap( A a, B b, C c, D d, E e ) : T(a, b, c, d, e) {}
+        template<typename A, typename B, typename C, typename D, typename E, typename F> StreamExWrap( A a, B b, C c, D d, E e, F f ) : T(a, b, c, d, e, f) {}
+        template<typename A, typename B, typename C, typename D, typename E, typename F, typename G> StreamExWrap( A a, B b, C c, D d, E e, F f, G g ) : T(a, b, c, d, e, f, g) {}
+        template<typename A, typename B, typename C, typename D, typename E, typename F, typename G, typename H> StreamExWrap( A a, B b, C c, D d, E e, F f, G g, H h ) : T(a, b, c, d, e, f, g, h) {}
+
+        template<typename A> StreamExWrap( A &a ) : T(a) {}
+        template<typename A, typename B> StreamExWrap( A &a, B &b ) : T(a, b) {}
+        template<typename A, typename B, typename C> StreamExWrap( A &a, B &b, C &c ) : T(a, b, c) {}
+        template<typename A, typename B, typename C, typename D> StreamExWrap( A &a, B &b, C &c, D &d ) : T(a, b, c, d) {}
+        template<typename A, typename B, typename C, typename D, typename E> StreamExWrap( A &a, B &b, C &c, D &d, E &e ) : T(a, b, c, d, e) {}
+        template<typename A, typename B, typename C, typename D, typename E, typename F> StreamExWrap( A &a, B &b, C &c, D &d, E &e, F &f ) : T(a, b, c, d, e, f) {}
+        template<typename A, typename B, typename C, typename D, typename E, typename F, typename G> StreamExWrap( A &a, B &b, C &c, D &d, E &e, F &f, G &g ) : T(a, b, c, d, e, f, g) {}
+        template<typename A, typename B, typename C, typename D, typename E, typename F, typename G, typename H> StreamExWrap( A &a, B &b, C &c, D &d, E &e, F &f, G &g, H &h ) : T(a, b, c, d, e, f, g, h) {}
+
+        /***
+            printf function.
+            Pass through allowing use of PrintExtension::printf.
+        ***/
+
+        pft PRINTF_ALIAS( const char *format, ... ){
+            va_list vList;
+            va_start( vList, format );
+            PrintEx p = *this;
+            const pft p_Return = p._printf( format, vList );
+            va_end( vList );
+            return p_Return;
+        }
+
+        #ifndef PRINTEX_NO_PROGMEM
+            pft PRINTF_ALIAS( const __FlashStringHelper *format, ... ){
+                va_list vList;
+                va_start( vList, format );
+                PrintEx p = *this;
+                const pft p_Return = p._printf( format, strlen_P((const char*)format)+1, vList );
+                va_end( vList );
+                return p_Return;
+            }
+        #endif
+
+    };
 #endif
